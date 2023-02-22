a00894ae11bf7cce5344e49f57b7dfe0a00ecc00
Kekeletsoncheeta <kekeletsoncheeta2@gmail.com>
2023-02-22 03:02:52 +0200

    g

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,48 +1,23 @@
-#include <stdio.h>
-#include <math.h>
+#include "holberton.h"
 
 int main(void)
 {
-	int a = 1, b = 2, c, i;
+	int a = 0, b = 1, c, i, num;
 
-	putchar('1');
-	putchar(',');
-	putchar(' ');
-	putchar('2');
-	putchar(',');
-	putchar(' ');
+	_putchar('Enter the number of terms: ');
+	scanf("%d", &num);
+	_putchar('\n');
 
-	for (i = 2; i < 98; i++)
+	_putchar('Fibonacci Series: ');
+	for (i = 1; i <= num; ++i)
 {
+	_putchar(a + '0');
+	_putchar(',');
+	_putchar(' ');
 	c = a + b;
 	a = b;
 	b = c;
-
-	int num = c;
-	int digits = 0;
-
-	while (num != 0)
-{
-	num /= 10;
-	digits++;
 }
 
-	num = c;
-	while (digits > 0)
-{
-	int digit = num / pow(10, digits - 1);
-	putchar(digit + '0');
-	num -= digit * pow(10, digits - 1);
-	digits--;
-}
-
-	if (i != 97)
-{
-	putchar(',');
-	putchar(' ');
-}
-}
-
-	putchar('\n');
 	return (0);
 }
